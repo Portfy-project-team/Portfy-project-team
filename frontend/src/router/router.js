0c5bd6a5b77e147c00bc7fc02a087feb7d36cde5
yasmina <ton_email_gitlab@gmail.com>
2026-04-28 22:57:52 +0100

    Add frontend from feature/auth

---
+++ frontend/src/router/router.js
@@ -0,0 +1,23 @@
+import { createRouter, createWebHistory } from 'vue-router'
+import LoginView from '../pages/auth/Login.vue'
+
+const routes = [
+  { path: '/', redirect: '/login' },
+  { 
+    path: '/login', 
+    name: 'login', 
+    component: LoginView 
+  },
+  { 
+    path: '/register', 
+    name: 'register', 
+    component: () => import('../pages/auth/Register.vue') 
+  }
+]
+
+const router = createRouter({
+  history: createWebHistory(),
+  routes
+})
+
+export default router
